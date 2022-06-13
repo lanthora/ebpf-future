21bd6464289fe1516a3e93c0d96fb0d3c877c340
lanthora <lanthora@outlook.com>
2022-06-13 07:22:37 +0000

    调整输出

--- entry-rets-address/sample.c
+++ entry-rets-address/sample.c
@@ -9,22 +9,20 @@ int main(int argc, char *argv[])
 	int error = 0;
 	struct uprobe_specific spec;
 
-	if (argc < 3)
-		return ERROR_INVALID;
+	assert(argc == 3);
 
 	spec.bin = argv[1];
 	spec.sym = argv[2];
 
 	error = uprobe_specific_analyze(&spec);
-	if (error)
-		return error;
+	assert(!error);
 
 	printf("bin: %s\n", spec.bin);
 	printf("sym: %s\n", spec.sym);
 	printf("entry: %p\n", spec.entry);
 	printf("rets: %p", spec.rets[0]);
 	for (int idx = 1; idx < LIB_UPROBE_RET_MAX && spec.rets[idx]; ++idx)
-		printf(", %p", spec.rets[idx]);
+		printf(" %p", spec.rets[idx]);
 	printf("\n");
 	return 0;
 }
